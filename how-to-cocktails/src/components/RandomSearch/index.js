3d1d6a9499210c21b25ee0c47ed098e34a3da8a6
Carlos E Alford <carlos.alford@gmail.com>
2022-06-17 14:35:25 +0100

    RandomSearch component complete

---
+++ how-to-cocktails/src/components/RandomSearch/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function RandomSearch({handleClick}) {
+  async function fetchAPI() {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const data = await response.json();
+    handleClick(data);
+  }
+
+  return (
+    <button onClick={fetchAPI}>RANDOM COCKTAIL</button>
+  )
+}
+
+export default RandomSearch;
