3c938eeed379bd5a237e096d6e3dc50d5c4b45f2
Carlos E Alford <carlos.alford@gmail.com>
2022-06-17 14:23:27 +0100

    Create basic App component

--- how-to-cocktails/src/components/App/App.js
+++ how-to-cocktails/src/components/App/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import RandomSearch from '../RandomSearch';
+
 
 function App() {
+  const [cocktails, setCocktails] = useState([]);
+
+  function updateCocktail(drink) {
+    setCocktails([drink]);
+  }
+
+  useEffect(() => {
+    async function fetchAPI() {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      const data = await response.json();
+      setCocktails([data.drinks]);
+    }
+    fetchAPI();
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1>HOW TO COCKTLS</h1>
+      <RandomSearch handleClick={updateCocktail} />
     </div>
   );
 }
