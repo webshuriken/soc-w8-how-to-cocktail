472b5ccd05ba000b6dbfafb20e49f918f193dbde
Carlos E Alford <carlos.alford@gmail.com>
2023-04-12 08:38:37 +0100

    Extend theme colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js, jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'razzmatazz': '#F31770',
+        'cocktail-fuchsia': '#F71FF9',
+        'white': '#ffffff',
+        'screaming-green': {
+          100: '#f0ffeb',
+          200: '#d1ffc2',
+          300: '#b3fe9a',
+          400: '#8FFE6B',
+        },
+        'licorice': '#110411',
+      },
+    },
   },
   plugins: [],
 }
