b32f177cbf58daaca6e32cf51a707f9231b8f084
Carlos E Alford <carlos.alford@gmail.com>
2023-04-12 13:14:10 +0100

    Add further custom theme shadows and shades for colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js, jsx}"],
   theme: {
+    boxShadow: {
+      'screaming-green': '0 0px 10px 1px rgba(87, 254, 32, 0.4), 0 0px 2px 1px rgba(87, 254, 32, 0.6)',
+      'razzmatazz': '0 0px 10px 1px rgba(243, 23, 112, 0.4), 0 0px 2px 1px rgba(243, 23, 112, 0.6)',
+      'cocktail-fuchsia': '0 0px 10px 1px rgba(247, 31, 249, 0.4), 0 0px 2px 1px rgba(247, 31, 249, 0.6)',
+    },
     extend: {
       colors: {
-        'razzmatazz': '#F31770',
-        'cocktail-fuchsia': '#F71FF9',
+        'razzmatazz': {
+          100: '#fdd8e7',
+          200: '#f98bb7',
+          300: '#f65193',
+          400: '#F31770',
+        },
+        'cocktail-fuchsia': {
+          100: '#fdd7fe',
+          200: '#fb9cfc',
+          300: '#f860fb',
+          400: '#F71FF9',
+        },
         'white': '#ffffff',
         'screaming-green': {
           100: '#f0ffeb',
-          200: '#d1ffc2',
-          300: '#b3fe9a',
-          400: '#8FFE6B',
+          200: '#b3fe9a',
+          300: '#85fe5d',
+          400: '#57fe20',
         },
         'licorice': '#110411',
       },
