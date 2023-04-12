cf041cc9c86d54189da494b5dd7875bdc6827e85
Carlos E Alford <carlos.alford@gmail.com>
2023-04-12 08:41:13 +0100

    css: setup layout for App container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ import SearchByName from '../SearchByName';
 function App() {
   const [cocktails, setCocktails] = useState([]);
 
-console.log(cocktails[0])
+console.log(cocktails);
 
   function updateCocktail(drink) {
     console.log(drink)
@@ -19,17 +19,25 @@ console.log(cocktails[0])
     async function fetchAPI() {
       const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
       const data = await response.json();
-      setCocktails(data.drinks);
+      setCocktails(drinks);
     }
     fetchAPI();
   }, []);
 
   return (
-    <div className="App">
-      <SearchByName handleClick={updateCocktail} />
-      <RandomSearch handleClick={updateCocktail} />
-      <h1>HOW TO COCKTAILS</h1>
+    <div className="container px-4 mx-auto bg-licorice text-white">
+      <header>
+        {/* TODO: WORKING ON THE HEADER TEXT */}
+        <h1 className="font-bold text-screaming-green-100 text-shadow-green-neon">HOW TO COCKTAILS</h1>
+        <SearchByName handleClick={updateCocktail} />
+        <RandomSearch handleClick={updateCocktail} />
+      </header>
+      <main>
       {cocktails[0] !== undefined && <DisplayCocktail cocktail={cocktails[0]} />}
+      </main>
+      <footer>
+        <p className="pt-4 pb-2 text-center">All rights reserved 2022-23</p>
+      </footer>
     </div>
   );
 }
