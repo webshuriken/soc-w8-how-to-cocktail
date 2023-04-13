70ebf20411e217e16bace78a32dbf4d469e61087
Carlos E Alford <carlos.alford@gmail.com>
2023-04-13 16:16:00 +0100

    style: improved the shadow effect for the screaming green color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./src/**/*.{js, jsx}"],
   theme: {
     boxShadow: {
-      'screaming-green': '0 0px 10px 1px rgba(87, 254, 32, 0.4), 0 0px 2px 1px rgba(87, 254, 32, 0.6)',
-      'inner-screaming-green': 'inset 0 0px 10px 1px rgba(87, 254, 32, 0.4), inset 0 0px 2px 1px rgba(87, 254, 32, 0.6)',
+      'screaming-green': '0 0px 10px 1px rgba(87, 254, 32, 0.4), 0 0px 4px 1px rgba(87, 254, 32, 0.6)',
+      'inner-screaming-green': 'inset 0 0px 10px 1px rgba(87, 254, 32, 0.4), inset 0 0px 4px 1px rgba(87, 254, 32, 0.6)',
       'razzmatazz': '0 0px 10px 1px rgba(243, 23, 112, 0.4), 0 0px 2px 1px rgba(243, 23, 112, 0.6)',
       'inner-razzmatazz': 'inset 0 0px 10px 1px rgba(243, 23, 112, 0.4), inset 0 0px 2px 1px rgba(243, 23, 112, 0.6)',
       'cocktail-fuchsia': '0 0px 10px 1px rgba(247, 31, 249, 0.4), 0 0px 2px 1px rgba(247, 31, 249, 0.6)',
