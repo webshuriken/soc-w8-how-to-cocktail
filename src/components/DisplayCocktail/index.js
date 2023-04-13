8ac373ed6da6af867e8a47bf0ad32a3be30f3f94
Carlos E Alford <carlos.alford@gmail.com>
2023-04-13 14:18:08 +0100

    Cocktail display components are now tailwindcss

--- src/components/DisplayCocktail/index.js
+++ src/components/DisplayCocktail/index.js
@@ -14,11 +14,35 @@ function DisplayCocktail({cocktail, updateCocktail}) {
   }
 
   return (
-    <section className="cocktail">
-      <h1>{cocktail.strDrink}</h1>
-      <Image src={cocktail.strDrinkThumb} />
-      {/* <IngredientList ingredient={ingredient} measure={measure} /> */}
-      <Instructions instructions={cocktail.strInstructions} />
+    <section>
+      <header className='flex'>
+        <div className='relative border-t-2 border-r-2 border-l-2 border-cocktail-fuchsia-100 rounded-t-lg shadow-cocktail-fuchsia overflow-hidden'>
+          <span className='block absolute -inset-0 rounded-t-md shadow-inner-cocktail-fuchsia' aria-hidden="true"></span>
+          <h1 className='relative p-2 text-white text-shadow-cocktail-fuchsia-neon'>
+            {cocktail.strDrink}
+          </h1>
+        </div>
+      </header>
+      <div className='flex flex-col md:flex-row gap-4'>
+        <div className='relative w-full max-w-[420px] md:w-1/3 border-2 border-cocktail-fuchsia-100 rounded-b-lg rounded-tr-lg p-4 shadow-cocktail-fuchsia overflow-hidden'>
+          <span className='block absolute -inset-0 rounded-b-md rounded-tr-md shadow-inner-cocktail-fuchsia' aria-hidden="true"></span>
+          <Image className="" 
+            src={cocktail.strDrinkThumb} />
+        </div>
+        <div className='flex flex-col gap-4 md:w-2/3 text-white'>
+          <div className='relative border-2 border-cocktail-fuchsia-100 rounded-lg p-4 shadow-cocktail-fuchsia overflow-hidden'>
+          <span className='block absolute -inset-0 rounded-md shadow-inner-cocktail-fuchsia' aria-hidden="true"></span>
+            {/* TODO: IMPLEMENT THE LOGIC TO DISPLAY THE INGREDIENTS */}
+            <h3 className='text-shadow-cocktail-fuchsia-neon font-bold'>Ingredients</h3>
+            {/* <IngredientList ingredient={ingredient} measure={measure} /> */}
+          </div>
+          <div className='relative border-2 border-cocktail-fuchsia-100 rounded-lg p-4 shadow-cocktail-fuchsia overflow-hidden'>
+            <span className='block absolute -inset-0 rounded-md shadow-inner-cocktail-fuchsia' aria-hidden="true"></span> 
+            <h3 className='text-shadow-cocktail-fuchsia-neon font-bold'>Instructions</h3>
+            <Instructions instructions={cocktail.strInstructions} />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/SearchByName/index.js
+++ src/components/SearchByName/index.js
@@ -18,7 +18,7 @@ function SearchByName({handleClick}) {
     <div className='flex gap-x-2 max-sm:mb-1'>
       <label className='w-3/4' htmlFor="search-by-name">
         <input
-          className='w-full border-2 border-screaming-green-400 rounded mr-1 p-1' 
+          className='w-full border-2 border-screaming-green-400 rounded mr-1 p-1 text-licorice' 
           id="search-by-name" 
           type="text" onChange={handleChange} 
           placeholder='Search by name' />
