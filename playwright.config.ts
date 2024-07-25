fc0acc6976051f27dbe45c2fd40ca4e7ee4c1ebe
webshuriken <webshuriken@gmail.com>
2024-07-25 18:41:12 +0100

    add WebServer option to the playwright config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,10 +43,11 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // Fedora linux is not fully supported by default
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -70,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http:127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
